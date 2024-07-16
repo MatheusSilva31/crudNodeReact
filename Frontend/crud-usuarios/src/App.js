5480736cd0fe3c7998bf3404ba34d0b0ce2e57d8
MatheusSilva31 <marheus.santos-silva@hotmail.com>
2024-07-16 16:18:22 -0300

    consumindo get usuarios

--- Frontend/crud-usuarios/src/App.js
+++ Frontend/crud-usuarios/src/App.js
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import { toast, ToastContainer } from "react-toastify";
 import Form from "./components/Form.js";
 
-import 'react-toastify/dist/ReactToastify.css'; // Importa o CSS do react-toastify
+import "react-toastify/dist/ReactToastify.css"; // Importa o CSS do react-toastify
+import Grid from "./components/Grid.js";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100vw;
@@ -18,13 +21,35 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res?.data.sort((a,b) => (a.nome > b.nome ? 1 : -1)));
+      
+    } catch (error) {
+      toast.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    getUsers()
+  },[setUsers])
   return (
     <>
       <Container>
         <Title> Crud Usuários </Title>
         <Form />
+        <Grid  users={users}/>
       </Container>
-      <ToastContainer autoClose={3000} position="bottom-left" />
+      <ToastContainer
+        autoClose={3000}
+        // position={toast.position.bottom_left}
+        position="bottom-left"
+      />
       <GlobalStyle />
     </>
   );
