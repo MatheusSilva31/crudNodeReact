a1abc96f4f3169eb508c97611ed031ea2f91f23d
MatheusSilva31 <marheus.santos-silva@hotmail.com>
2024-07-16 15:28:02 -0300

    criação do formulario

--- Frontend/crud-usuarios/src/components/Form.js
+++ Frontend/crud-usuarios/src/components/Form.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
 
-const Form = () =>{
-    return (
-        <h1>ola</h1>
-    )
-}
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 0px #ccc;
+  border-radius: 5px;
+`;
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-export default Form
+const Label = styled.label``;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Button = styled.button`
+padding:10px;
+cursor: pointer;
+border-radius: 5px;
+border: none;
+background-color: #2c73d2;
+color: white;
+height: 42px;
+`
+
+const Form = ({ ondEdit }) => {
+  const ref = useRef();
+
+  return (
+    <FormContainer ref={ref}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email"  type="email"/>
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="telefone" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de nascimento</Label>
+        <Input name="data_nascimento" type="date"/>
+      </InputArea>
+
+      <Button type="submit">Salvar </Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
