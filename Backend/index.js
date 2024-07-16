ea458476b0546968a7d898a96c3f59806b614fbf
MatheusSilva31 <marheus.santos-silva@hotmail.com>
2024-07-16 11:35:33 -0300

    instalacao de eslint

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-import userRoutes from './routes/user.js'
+import userRoutes from "./routes/user.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/',userRoutes)
+app.use("/", userRoutes);
 
-app.listen(8800)
+app.listen(8800);
