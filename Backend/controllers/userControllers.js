be80c356fb5283509b17eadfdb5e262e4241d6d0
MatheusSilva31 <marheus.santos-silva@hotmail.com>
2024-07-23 10:08:05 -0300

    novos endpoint e backend atualizado

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -12,7 +12,7 @@ export const getUser = (_, res) => {
 
 export const addUser = (req, res) => {
   const q =
-    "INSERT INTO usuarios ('nome,'telefone','data_nascimento','email') VALUES(?)";
+    "INSERT INTO usuarios (nome,telefone,data_nascimento,email) VALUES(?)";
 
   const values = [
     req.body.nome,
@@ -22,7 +22,7 @@ export const addUser = (req, res) => {
   ];
 
   db.query(q, [values], (err) => {
-    if (err) return res.json(err);
+    if (err) return res.status(404).json(err);
 
     return res.status(200).json("Usuário criado com sucesso");
   });
@@ -30,7 +30,7 @@ export const addUser = (req, res) => {
 
 export const updateUser = (req, res) => {
   const q =
-    "UPDATE usuarios set 'nome' = ? ,'telefone' = ?, 'data_nascimento' = ?, 'email' = ? WHERE 'id' = ?";
+    "UPDATE usuarios set nome = ? ,telefone = ?, data_nascimento = ?, email = ? WHERE idusuarios = ?";
 
   const values = [
     req.body.nome,
@@ -39,21 +39,92 @@ export const updateUser = (req, res) => {
     req.body.email,
   ];
 
-  db.query(q, [...values,req.params.id], (err) => {
-    if (err) return res.json(err);
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.status(404).json(err);
 
     return res.status(200).json("Usuário atualizado com sucesso");
   });
 };
 
-
 export const deleteUser = (req, res) => {
-    const q =
-      "DELETE FROM usuarios  WHERE 'id' = ?";
-  
-    db.query(q, [req.params.id], (err) => {
-      if (err) return res.json(err);
-  
-      return res.status(200).json("Usuário deletado com sucesso");
-    });
-  };
+  const q = "DELETE FROM usuarios  WHERE idusuarios = ?";
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.status(404).json(err);
+    return res.status(200).json("Usuário deletado com sucesso");
+  });
+};
+
+export const getUserDetails = (req, res) => {
+  const q = `
+      SELECT 
+        u.nome,
+        e.rua,
+        e.numero,
+        e.cidade,
+        p.nome AS pais
+      FROM 
+        enderecos AS e
+      INNER JOIN 
+        usuarios AS u ON e.idusuario = u.idusuarios
+      INNER JOIN 
+        paises AS p ON e.idpais = p.idpais
+      WHERE 
+        u.idusuarios = ?
+    `;
+
+  db.query(q, [req.params.id], (err, result) => {
+    if (err) return res.status(404).json(err);
+
+    if (result.length > 0) {
+      return res.status(200).json(result[0]); // Retorna apenas o primeiro objeto
+    } else {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+  });
+};
+
+export const addAddress = (req, res) => {
+  const q =
+    "INSERT INTO enderecos (rua, numero, cidade, idpais, idusuario) VALUES (?)";
+
+  const values = [
+    req.body.rua,
+    req.body.numero,
+    req.body.cidade,
+    req.body.idpais,
+    req.body.idusuario,
+  ];
+
+  console.log(values);
+  db.query(q, [values], (err) => {
+    if (err) return res.status(404).json(err);
+
+    return res.status(200).json("Endereço criado com sucesso");
+  });
+};
+
+export const updateAddress = (req, res) => {
+  const q =
+    "UPDATE enderecos set rua = ?, numero = ?, cidade = ? WHERE idusuario = ?";
+
+  const values = [req.body.rua, req.body.numero, req.body.cidade];
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.status(404).json(err);
+
+    return res.status(200).json("Endereço atualizado com sucesso");
+  });
+};
+
+export const addCountry = (req, res) => {
+  const q = "INSERT INTO paises (nome) VALUES (?)";
+
+  const values = [req.body.nome];
+
+  console.log(values);
+  db.query(q, [values], (err) => {
+    if (err) return res.status(404).json(err);
+
+    return res.status(200).json("País criado com sucesso");
+  });
+};
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUser,addUser,updateUser,deleteUser } from '../controllers/userControllers.js'
+import { getUser,addUser,updateUser,deleteUser, getUserDetails, addAddress,updateAddress,addCountry } from '../controllers/userControllers.js'
 
 const router = express.Router()
 
@@ -10,5 +10,13 @@ router.post('/',addUser)
 router.put('/:id',updateUser)
 
 router.delete('/:id',deleteUser)
+
+router.get('/user/:id',getUserDetails)
+
+router.post('/user/address',addAddress)
+
+router.put('/user/address/:id',updateAddress)
+
+router.post('/country',addCountry)
 
 export default router
